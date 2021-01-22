e18ce1a47d82d0b7c3723503fcc630705673c023
Sourabh-Deshmukh <sourabh1196deshmukh@gmail..com>
2021-01-22 16:25:05 +0530

    Empty string implementation added

--- src/main/java/io/sdWorld/StringCalculator.java
+++ src/main/java/io/sdWorld/StringCalculator.java
@@ -3,6 +3,9 @@ package io.sdWorld;
 public class StringCalculator {
 
     public int add(String numbers){
+        if(numbers.isEmpty()){
+            return 0;
+        }
         return 123;
     }
 
