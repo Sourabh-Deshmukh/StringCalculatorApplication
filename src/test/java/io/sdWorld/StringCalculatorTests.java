4228df0602ea1eacc6c61409cf5f602ed0a4dfc5
Sourabh-Deshmukh <sourabh1196deshmukh@gmail..com>
2021-01-22 16:40:12 +0530

    Test case : two numbers

--- src/test/java/io/sdWorld/StringCalculatorTests.java
+++ src/test/java/io/sdWorld/StringCalculatorTests.java
@@ -1,23 +1,34 @@
 package io.sdWorld;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTests {
 
-    private static  final String EMPTY = "";
+    private static final String EMPTY = "";
+    private static StringCalculator stringCalculator;
+
+    @BeforeClass
+    public static void initialSetUp(){
+        stringCalculator = new StringCalculator();
+    }
 
     @Test
     public void testAddWithEmptyString(){
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(EMPTY));
     }
 
     @Test
     public void testAddWithOneNumber(){
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
+    }
+
+    @Test
+    public void testAddWithTwoNumber(){
+        assertEquals(1, stringCalculator.add("1,2"));
     }
 
 }
